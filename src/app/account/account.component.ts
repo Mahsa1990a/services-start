29ac13cf95ded695f2c61cf66aac5ef8b0c6dd8a
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-28 09:09:10 -0400

    108. Injecting the Logging Service into Components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  // 2.after adding this: angular would know how to give us an instance
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  // 1.inform angular we will need instance of LoggingService class(loggingService property with type of LoggingService class)
+  constructor(private loggingService: LoggingService) {}
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    // console.log('A server status changed, new status: ' + status); UPDATE:
+   // 3.now access service:
+   this.loggingService.logStatusChanged(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,10 +4,15 @@ import { LoggingService } from "../logging.service";
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // 2.after adding this: angular would know how to give us an instance
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  //1. inform angular we will need instance of LoggingService class(loggingService property with type of LoggingService class)
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -15,8 +20,11 @@ export class NewAccountComponent {
       status: accountStatus
     });
 
+    // 3.now access service:
+    this.loggingService.logStatusChanged(accountStatus);
+
     // console.log('A server status changed, new status: ' + accountStatus); UPDATE with services
-    const service = new LoggingService();
-    service.logStatusChanged(accountStatus);
+    // const service = new LoggingService();
+    // service.logStatusChanged(accountStatus); update instead of access service manually
   }
 }
